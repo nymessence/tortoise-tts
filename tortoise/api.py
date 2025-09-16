6dc2436f9fa4c2074f8febb4513f28a700115945
humid_ray_0u@icloud.com <nymessence>
2025-09-16 18:57:15 -0400

    adjust default parameters for near realtime rendering on TPU while maintaing quailty (may fine tune later if needed)

--- tortoise/api.py
+++ tortoise/api.py
@@ -383,14 +383,14 @@ class TextToSpeech:
         settings.update(kwargs) # allow overriding of preset settings with kwargs
         return self.tts(text, **settings)
 
-    def tts(self, text, voice_samples=None, conditioning_latents=None, k=8, verbose=True, use_deterministic_seed=None,
+    def tts(self, text, voice_samples=None, conditioning_latents=None, k=1, verbose=True, use_deterministic_seed=None,
                 return_deterministic_state=False,
                 # autoregressive generation parameters follow
                 num_autoregressive_samples=8, temperature=.8, length_penalty=1, repetition_penalty=2.0, top_p=.8, max_mel_tokens=500,
                 # CVVP parameters follow
                 cvvp_amount=.0,
                 # diffusion generation parameters follow
-                diffusion_iterations=100, cond_free=True, cond_free_k=2, diffusion_temperature=1.0,
+                diffusion_iterations=16, cond_free=False, cond_free_k=2, diffusion_temperature=1.0,
                 **hf_generate_kwargs):
             """
             Produces an audio clip of the given text being spoken with the given reference voice.
