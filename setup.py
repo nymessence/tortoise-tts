937a16725747f0f2b51d172552aab7e022879c44
humid_ray_0u@icloud.com <nymessence>
2025-09-19 13:29:23 -0400

    update

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="tortoise-tts",
     packages=setuptools.find_packages(),
-    version="3.0.0",
-    author="James Betker",
-    author_email="james@adamant.ai",
-    description="A high quality multi-voice text-to-speech library",
+    version="3.1.0 beta",
+    author="Nymessence",
+    author_email="no official email yet",
+    description="A high quality multi-voice text-to-speech library (nymessence fork)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/neonbjb/tortoise-tts",
+    url="https://github.com/nymessence/tortoise-tts",
     project_urls={},
     scripts=[
         'scripts/tortoise_tts.py',
@@ -27,9 +27,9 @@ setuptools.setup(
         'unidecode',
         'scipy',
         'librosa',
-        'transformers==4.31.0',
-        'tokenizers==0.14.0',
-        'scipy==1.13.1'
+        'transformers>=4.31.0',
+        'tokenizers>=0.14.0',
+        'scipy>=1.13.1'
         # 'deepspeed==0.8.3',
     ],
     classifiers=[
